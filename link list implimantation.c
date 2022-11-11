c66223ac31f76db8d9a1d7fac8449a36f2483874
RahulGajareUG24 <rahul.gajare_ug24@ashoka.edu.in>
2022-11-11 15:57:46 +0530

    commiting link list implimantation.c

--- link list implimantation.c
+++ link list implimantation.c
@@ -1,1 +1,85 @@
   
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int num;                        //Data of the node
+    struct node *nextptr;           //Address of the next node
+};
+typedef struct node * NodeAddress;
+
+NodeAddress createLinkList(int n)
+{
+	NodeAddress fnNode= NULL
+	NodeAddress tmp= NULL;
+    int num, i;
+	NodeAddress hed = (struct node *)malloc(sizeof(struct node));
+
+    if(hed == NULL) //check whether the fnnode is NULL and if so no memory allocation
+    {
+        printf(" Memory can not be allocated.");
+    }
+    else
+    {
+// reads data for the node through keyboard
+
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+        hed->num = num;      
+        hed->nextptr = NULL; // links the address field to NULL
+        tmp = hed;
+// Creating n nodes and adding to linked list
+        for(i=2; i<=n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL)
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+ 
+                fnNode->num = num;      // links the num field of fnNode with num
+                fnNode->nextptr = NULL; // links the address field of fnNode with NULL
+ 
+                tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
+                tmp = tmp->nextptr; 
+            }
+        }
+    }
+}
+NodeADDRESS printList()
+{
+    NodeADDRESS tmp;
+    if(hed == NULL)
+    {
+        printf(" List is empty.");
+    }
+    else
+    {
+        tmp = hed;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num);       // prints the data of current node
+            tmp = tmp->nextptr;                     // advances the position of current node
+        }
+    }
+} 
+
+int main()
+{
+    int n;
+		printf("\n\n Linked List : To create and display Singly Linked List :\n");
+		printf("-------------------------------------------------------------\n");
+		
+    printf(" Input the number of nodes : ");
+    scanf("%d", &n);
+    createNodeList(n);
+    printf("\n Data entered in the list : \n");
+    displayList();
+    return 0;
+}
